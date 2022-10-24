e61170426158253bc72f6989fc521fc76e3be0bd
LukeWebster-Khan <Luke.Webster-Khan@theaccesspoint.co.uk>
2022-10-24 20:31:36 +0100

    added about json and function to toggle description

--- components/About.js
+++ components/About.js
@@ -1,31 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../styles/Page.module.scss";
-
+import data from "../public/about.json";
 const About = () => {
+  const [active, setActive] = useState(0);
+  const onChange = (e) => {
+    setActive(e.target.getAttribute("data-index"));
+  };
   return (
     <section className={styles.about} id="about">
       <h1>About</h1>
-
       <div className={styles.about__container}>
         <div className={styles.about__buttons}>
-          <button className={styles.about__button}>Born</button>
-          <button className={styles.about__button}>Growing Up</button>
-          <button className={styles.about__button}>School</button>
-          <button className={styles.about__button}>Engineering</button>
-          <button className={styles.about__button}>Intro to Programming</button>
-          <button className={styles.about__button}>AccessPoint</button>
+          <button
+            onClick={onChange}
+            className={styles.about__button}
+            data-index="0"
+          >
+            Born
+          </button>
+          <button
+            onClick={onChange}
+            data-index="1"
+            className={styles.about__button}
+          >
+            Growing Up
+          </button>
+          <button
+            onClick={onChange}
+            data-index="2"
+            className={styles.about__button}
+          >
+            School
+          </button>
+          <button
+            onClick={onChange}
+            data-index="3"
+            className={styles.about__button}
+          >
+            Engineering
+          </button>
+          <button
+            onClick={onChange}
+            data-index="4"
+            className={styles.about__button}
+          >
+            Intro to Programming
+          </button>
+          <button
+            onClick={onChange}
+            data-index="5"
+            className={styles.about__button}
+          >
+            AccessPoint
+          </button>
         </div>
         <div className={styles.about__content}>
-          <p className={styles.about__desc}>
-            After finishing secondary school at <a>Clifton College</a> I pursued
-            a career in Civil Engineering, believing it to be an avenue I could
-            thrive in. After 4 years im the industry I was proven wrong.
-            However, soon enough I caught a glimpse of the world of software
-            development and I was hooked. As a proactive self starter I began
-            learing HTML, CSS and JavaScript. I particularly have an affinity
-            for both the creative styalistic expression through SCSS and the
-            logical problem solving of JavaScript.
-          </p>
+          <p className={styles.about__desc}>{data[active].desc}</p>
         </div>
       </div>
     </section>
