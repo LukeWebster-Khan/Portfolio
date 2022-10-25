c1c32134ae04ec4d75d4e12bc592cf1245b83a01
LukeWebster-Khan <Luke.Webster-Khan@theaccesspoint.co.uk>
2022-10-25 23:20:00 +0100

    adjusted framer motion and removed csstransition

--- components/About.js
+++ components/About.js
@@ -1,81 +1,80 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import styles from "../styles/Page.module.scss";
 import data from "../public/about.json";
 import { motion } from "framer-motion";
-import { CSSTransition } from "react-transition-group";
 
 const About = () => {
   const [active, setActive] = useState(0);
-
+  const [showMessage, setShowMessage] = useState(true);
+  const nodeRef = useRef(null);
   const onChange = (e) => {
     setActive(e.target.getAttribute("data-index"));
   };
   return (
-    <CSSTransition key={1} timeout={250}>
-      <section className={styles.about} id="about">
-        <h1>About</h1>
-        <div className={styles.about__container}>
-          <div className={styles.about__buttons}>
-            <button
-              onClick={onChange}
-              className={styles.about__button}
-              data-index="0"
-            >
-              Born
-            </button>
-            <button
-              onClick={onChange}
-              data-index="1"
-              className={styles.about__button}
-            >
-              Growing Up
-            </button>
-            <button
-              onClick={onChange}
-              data-index="2"
-              className={styles.about__button}
-            >
-              School
-            </button>
-            <button
-              onClick={onChange}
-              data-index="3"
-              className={styles.about__button}
-            >
-              Engineering
-            </button>
-            <button
-              onClick={onChange}
-              data-index="4"
-              className={styles.about__button}
-            >
-              Intro to Programming
-            </button>
-            <button
-              onClick={onChange}
-              data-index="5"
-              className={styles.about__button}
-            >
-              AccessPoint
-            </button>
-          </div>
-          <div className={styles.about__content}>
-            <motion.p
-              animate={{
-                x: 0,
-                opacity: 1,
-              }}
-              transition={{
-                duration: 1,
-              }}
-            >
-              {data[active].date}
-            </motion.p>
-            <p className={styles.about__desc}>{data[active].desc}</p>
-          </div>
+    <section className={styles.about} id="about">
+      <h1>About</h1>
+      <div className={styles.about__container}>
+        <div className={styles.about__buttons}>
+          <button
+            onClick={onChange}
+            className={styles.about__button}
+            data-index="0"
+          >
+            Born
+          </button>
+          <button
+            onClick={onChange}
+            data-index="1"
+            className={styles.about__button}
+          >
+            Growing Up
+          </button>
+          <button
+            onClick={onChange}
+            data-index="2"
+            className={styles.about__button}
+          >
+            School
+          </button>
+          <button
+            onClick={onChange}
+            data-index="3"
+            className={styles.about__button}
+          >
+            Engineering
+          </button>
+          <button
+            onClick={onChange}
+            data-index="4"
+            className={styles.about__button}
+          >
+            Intro to Programming
+          </button>
+          <button
+            onClick={onChange}
+            data-index="5"
+            className={styles.about__button}
+          >
+            AccessPoint
+          </button>
         </div>
-      </section>
-    </CSSTransition>
+        <motion.div
+          className={styles.about__content}
+          initial={{ x: -100, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{
+            delay: 0,
+            x: { duration: 1 },
+            default: { ease: "linear" },
+          }}
+        >
+          <div>
+            <p className={styles.about__desc}>{data[active].desc}</p>
+            <p>{data[active].date}</p>
+          </div>
+        </motion.div>
+      </div>
+    </section>
   );
 };
 export default About;
