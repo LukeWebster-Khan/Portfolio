b7c99dafd620fd6f3d4cacc4f497370b54dd5da1
LukeWebster-Khan <Luke.Webster-Khan@theaccesspoint.co.uk>
2022-10-27 00:24:08 +0100

    added spin effect for skills

--- components/Skills.js
+++ components/Skills.js
@@ -5,33 +5,43 @@ const Skills = () => {
   return (
     <div className={styles.skills}>
       <h1>Skills</h1>
-      <div className={styles.skills__wrapper}>
-        <div className={styles.skills__item}>
-          <Image
-            src="/html5.png"
-            alt="html"
-            objectFit="contain"
-            layout="fill"
-          />
-        </div>
-        <div className={styles.skills__item}>
-          <Image src="/SASS.png" alt="sass" objectFit="contain" layout="fill" />
-        </div>
-        <div className={styles.skills__item}>
-          <Image
-            src="/react.png"
-            alt="react"
-            objectFit="contain"
-            layout="fill"
-          />
-        </div>
-        <div className={styles.skills__item}>
-          <Image
-            src="/typescript.png"
-            alt="typescript"
-            objectFit="contain"
-            layout="fill"
-          />
+      <div className={styles.skills__container}>
+        <p className={styles.skills__desc}>
+          These are a list of the skills I have
+        </p>
+        <div className={styles.skills__wrapper}>
+          <div className={styles.skills__item}>
+            <Image
+              src="/html5.png"
+              alt="html"
+              objectFit="contain"
+              layout="fill"
+            />
+          </div>
+          <div className={styles.skills__item}>
+            <Image
+              src="/SASS.png"
+              alt="sass"
+              objectFit="contain"
+              layout="fill"
+            />
+          </div>
+          <div className={styles.skills__item}>
+            <Image
+              src="/react.png"
+              alt="react"
+              objectFit="contain"
+              layout="fill"
+            />
+          </div>
+          <div className={styles.skills__item}>
+            <Image
+              src="/typescript.png"
+              alt="typescript"
+              objectFit="contain"
+              layout="fill"
+            />
+          </div>
         </div>
       </div>
     </div>
