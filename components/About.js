18d01550213698b37c95a10c107942e70f77833c
LukeWebster-Khan <Luke.Webster-Khan@theaccesspoint.co.uk>
2022-11-03 20:09:52 +0000

    restyled about section

--- components/About.js
+++ components/About.js
@@ -89,24 +89,3 @@ const About = () => {
   );
 };
 export default About;
-
-{
-  /* <p className={styles.about__desc}>
-        Hi! I&apos;m Luke Webster-Khan. I&apos;m currently working in a Junior
-        Dev Role at AccessPoint Technologies. It took a while for me to find my
-        true passion of software development. I was born and raised in Bristol,
-        England as the youngest of 3. I&apos;ve always found joy in problem
-        solving and technical challenges but didn&apos;t know about the wonders
-        of programming.
-      </p>
-      <p className={styles.about__desc}>
-        After finishing secondary school at <a>Clifton College</a> I pursued a
-        career in Civil Engineering, believing it to be an avenue I could thrive
-        in. After 4 years im the industry I was proven wrong. However, soon
-        enough I caught a glimpse of the world of software development and I was
-        hooked. As a proactive self starter I began learing HTML, CSS and
-        JavaScript. I particularly have an affinity for both the creative
-        styalistic expression through SCSS and the logical problem solving of
-        JavaScript.
-      </p> */
-}
