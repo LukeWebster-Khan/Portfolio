2026d446ac8041f149da4ef7f731dc0d58efad31
LukeWebster-Khan <Luke.Webster-Khan@theaccesspoint.co.uk>
2023-01-21 11:47:04 +0000

    added framer motion animations

--- components/About.js
+++ components/About.js
@@ -1,9 +1,7 @@
 import React, { useState, useRef, useEffect } from "react";
 import styles from "../styles/Page.module.scss";
 import Image from "next/image";
-import data from "../public/about.json";
 import { motion } from "framer-motion";
-import { SwitchTransition, CSSTransition } from "react-transition-group";
 import useEmblaCarousel from "embla-carousel-react";
 
 const About = () => {
@@ -16,54 +14,28 @@ const About = () => {
   }, [emblaApi]);
 
   return (
-    <section className={styles.about} id="about">
-      <h1>About</h1>
-      <div className={styles.about__container}>
-        <div className={styles["about__content-wrapper"]}>
-          <div data-content="2" className={styles["about__section"]}>
-            <p>lorem ipsum and the ol second slide</p>
-            <div>
-              <Image
-                src="/bristol.jpg"
-                alt="location"
-                width={300}
-                height={300}
-              />
-            </div>
-          </div>
-          <div data-content="3" className={styles["about__section"]}>
-            <p>lorem ipsum</p>
-            <div>
-              <Image
-                src="/bristol.jpg"
-                alt="location"
-                width={300}
-                height={300}
-              />
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className={styles.embla} ref={emblaRef}>
-        <div className={styles.embla__container}>
-          <div className={styles.embla__slide}>
-            <div
-              data-content="1"
-              className={`${styles["about__section"]} ${styles["about__section--visible"]}`}
-            >
-              <div className={styles["about__section--desc"]}>
-                <h2>Myself</h2>
-                <p>
-                  Hello! My name is Luke and I love creating things that live on
-                  the internet. I&#39;m a full stack developer based in Bristol.
-                  I&#39;ve always had a love for techincal challenges.
-                  <br></br>
-                  <br></br>I worked in Civil Engineering for 4 years before
-                  making a career change. I&#39;ve always had a love for
-                  techincal challenges and strive to learn more everyday!
-                  <br></br>
-                </p>
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 0.5 }}
+    >
+      <section className={styles.about} id="about">
+        <h1>About</h1>
+        <div className={styles.about__container}>
+          <div className={styles["about__content-wrapper"]}>
+            <div data-content="2" className={styles["about__section"]}>
+              <p>lorem ipsum and the ol second slide</p>
+              <div>
+                <Image
+                  src="/bristol.jpg"
+                  alt="location"
+                  width={300}
+                  height={300}
+                />
               </div>
+            </div>
+            <div data-content="3" className={styles["about__section"]}>
+              <p>lorem ipsum</p>
               <div>
                 <Image
                   src="/bristol.jpg"
@@ -74,38 +46,71 @@ const About = () => {
               </div>
             </div>
           </div>
-          <div className={styles.embla__slide}>
-            <div
-              data-content="1"
-              className={`${styles["about__section"]} ${styles["about__section--visible"]}`}
-            >
-              <div className={styles["about__section--desc"]}>
-                <h2>Some Facts</h2>
-                <p>
-                  - I&rsquo;m well versed in using PHP to build custom themes
-                  for WordPress websites along with WebPack and Node for backend
-                  shinanigans
-                </p>
-                <br></br>
-                <p>
-                  - My favourite programming launguage to work with is{" "}
-                  <bold>React</bold> due to its simplicity and ease of use.
-                </p>
-              </div>
-              <div>
-                <Image
-                  src="/code.jpg"
-                  alt="location"
-                  width={300}
-                  height={300}
-                />
+        </div>
+        <div className={styles.embla} ref={emblaRef}>
+          <div className={styles.embla__container}>
+            <div className={styles.embla__slide}>
+              <div
+                data-content="1"
+                className={`${styles["about__section"]} ${styles["about__section--visible"]}`}
+              >
+                <div className={styles["about__section--desc"]}>
+                  <h2>Myself</h2>
+                  <p>
+                    Hello! My name is Luke and I love creating things that live
+                    on the internet. I&#39;m a full stack developer based in
+                    Bristol. I&#39;ve always had a love for techincal
+                    challenges.
+                    <br></br>
+                    <br></br>I worked in Civil Engineering for 4 years before
+                    making a career change. I&#39;ve always had a love for
+                    techincal challenges and strive to learn more everyday!
+                    <br></br>
+                  </p>
+                </div>
+                <div>
+                  <Image
+                    src="/bristol.jpg"
+                    alt="location"
+                    width={300}
+                    height={300}
+                  />
+                </div>
               </div>
             </div>
+            <div className={styles.embla__slide}>
+              <div
+                data-content="1"
+                className={`${styles["about__section"]} ${styles["about__section--visible"]}`}
+              >
+                <div className={styles["about__section--desc"]}>
+                  <h2>Some Facts</h2>
+                  <p>
+                    - I&rsquo;m well versed in using PHP to build custom themes
+                    for WordPress websites along with WebPack and Node for
+                    backend shinanigans
+                  </p>
+                  <br></br>
+                  <p>
+                    - My favourite programming launguage to work with is React
+                    due to its simplicity and ease of use.
+                  </p>
+                </div>
+                <div>
+                  <Image
+                    src="/code.jpg"
+                    alt="location"
+                    width={300}
+                    height={300}
+                  />
+                </div>
+              </div>
+            </div>
+            {/* <div className={styles.embla__slide}>third</div> */}
           </div>
-          {/* <div className={styles.embla__slide}>third</div> */}
         </div>
-      </div>
-    </section>
+      </section>
+    </motion.div>
   );
 };
 export default About;
--- components/Skills.js
+++ components/Skills.js
@@ -1,42 +1,50 @@
 import React, { useState, useRef } from "react";
 import styles from "../styles/Page.module.scss";
 import Image from "next/image";
+import { motion } from "framer-motion";
+
 const Skills = () => {
   return (
-    <div id="skills" className={styles.skills}>
-      <h1>Skills</h1>
-      <div className={styles.skills__container}>
-        <div className={styles["skills__desc-container"]}>
-          <p className={styles.skills__desc}>
-            Over the past year I have picked up a number of skills that I have
-            applied to a varied world of projects. I have worked with a number
-            of different technologies and frameworks, and I am always looking to
-            learn more.
-          </p>
-          <p className={styles.skills__desc}>
-            I utilise a number of different front-end technologies, including
-            React, Next.js, and Tailwind CSS. I have also worked with a number
-            of back-end technologies, including Node.js, Express, and MongoDB.
-          </p>
-        </div>
-        <div className={styles.skills__wrapper}>
-          <div className={styles.skills__item}>
-            <Image src="/html5.png" alt="html" layout="fill" />
+    <motion.div
+      initial={{ opacity: 0, x: -1000 }}
+      whileInView={{ opacity: 1, x: 0 }}
+      transition={{ duration: 0.5 }}
+    >
+      <div id="skills" className={styles.skills}>
+        <h1>Skills</h1>
+        <div className={styles.skills__container}>
+          <div className={styles["skills__desc-container"]}>
+            <p className={styles.skills__desc}>
+              Over the past year I have picked up a number of skills that I have
+              applied to a varied world of projects. I have worked with a number
+              of different technologies and frameworks, and I am always looking
+              to learn more.
+            </p>
+            <p className={styles.skills__desc}>
+              I utilise a number of different front-end technologies, including
+              React, Next.js, and Tailwind CSS. I have also worked with a number
+              of back-end technologies, including Node.js, Express, and MongoDB.
+            </p>
           </div>
-          <div className={styles.skills__item}>
-            <span>
-              <Image src="/SASS.png" alt="sass" layout="fill" />
-            </span>
-          </div>
-          <div className={styles.skills__item}>
-            <Image src="/react.png" alt="react" layout="fill" />
-          </div>
-          <div className={styles.skills__item}>
-            <Image src="/typescript.png" alt="typescript" layout="fill" />
+          <div className={styles.skills__wrapper}>
+            <div className={styles.skills__item}>
+              <Image src="/html5.png" alt="html" layout="fill" />
+            </div>
+            <div className={styles.skills__item}>
+              <span>
+                <Image src="/SASS.png" alt="sass" layout="fill" />
+              </span>
+            </div>
+            <div className={styles.skills__item}>
+              <Image src="/react.png" alt="react" layout="fill" />
+            </div>
+            <div className={styles.skills__item}>
+              <Image src="/typescript.png" alt="typescript" layout="fill" />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 };
 export default Skills;
